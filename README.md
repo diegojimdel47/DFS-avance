1046f228c7a34e189efd0b4b0da2b13455745b72
diegojimdel47 <diego.jimenez.del@gmail.com>
2026-01-30 21:39:04 -0600

    Índice creado

---
+++ README.md
@@ -0,0 +1,13 @@
+# Índice
+## Avance del reto
+[Ver documentación](avance_reto.md)
+
+[Ir a la rama](tree/avance-reto)
+## Ejercicios
+[Ver documentación](ejercicios.md)
+
+[Ir a la rama](/tree/ejercicios)
+## Reto final
+[Ver documentación](reto_final.md)
+
+[Ir a la rama](tree/reto-final)
---
+++ avance_reto.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class avance_reto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opt;
+
+        do {
+
+            System.out.println("Sistema de gestión de biblioteca");
+            System.out.println("\nMenú Princial");
+            System.out.println("1. Estado de la biblioteca");
+            System.out.println("2. Menú de usuarios");
+            System.out.println("3. Menú de libros");
+            System.out.println("4. Menú de prestamos");
+            System.out.println("5. Salir");
+
+            System.out.print("\nEscoge una opción: ");
+            opt = sc.nextInt();
+
+            switch(opt) {
+                case 1: 
+                    System.out.println("Estado de la biblioteca en proceso de creación...");
+                    break;
+                case 2:
+                    System.out.println("Menú de usuarios en proceso de creación...");
+                    break;
+                case 3:
+                    System.out.println("Menú de libros en proceso de creación...");
+                    break;
+                case 4:
+                    System.out.println("Menú de prestamos en proceso de creación...");
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción inválida, por favor intenta otra vez.");
+            }
+        }while(opt != 5);
+    }
+}
